97cacf65bdd6108c167ec9fd5f5035475cba27f5
ravetam <rave.tam@gmail.com>
2016-12-26 22:50:32 +0800

    Fixed bug with door sensor

--- components/switch/xiaomi.py
+++ components/switch/xiaomi.py
@@ -1,40 +1,68 @@
 """
 Support for Xiaomi binary sensors.
 
+Developed by Rave from Lazcad.com
 """
 import logging
 
+from homeassistant.helpers.entity import Entity
 from homeassistant.components.switch import (SwitchDevice)
-from homeassistant.components.xiaomi import (XiaomiDevice, XIAOMI_HUB)
 
 _LOGGER = logging.getLogger(__name__)
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Perform the setup for Xiaomi devices."""
     
-    for resp in XIAOMI_HUB.XIAOMI_DEVICES['switch']:
-        model = resp['model']
+    XIAOMI_HUB = hass.data['XIAOMI_HUB']
+    for device in XIAOMI_HUB.XIAOMI_DEVICES['switch']:
+        model = device['model']
         if (model == 'plug'):
-            add_devices([XiaomiGenericSwitch(resp, "Plug", 'status', XIAOMI_HUB)])
+            add_devices([XiaomiGenericSwitch(device, "Plug", 'status', XIAOMI_HUB)])
         elif (model == 'ctrl_neutral1'):
-            add_devices([XiaomiGenericSwitch(resp, 'Wall Switch', 'channel_0', XIAOMI_HUB)])
+            add_devices([XiaomiGenericSwitch(device, 'Wall Switch', 'channel_0', XIAOMI_HUB)])
         elif (model == 'ctrl_neutral2'):
             add_devices([
-                XiaomiGenericSwitch(resp, 'Wall Switch Left','channel_0', XIAOMI_HUB), 
-                XiaomiGenericSwitch(resp, 'Wall Switch Right', 'channel_1', XIAOMI_HUB)])
+                XiaomiGenericSwitch(device, 'Wall Switch Left','channel_0', XIAOMI_HUB), 
+                XiaomiGenericSwitch(device, 'Wall Switch Right', 'channel_1', XIAOMI_HUB)])
+
+class XiaomiDevice(Entity):
+    """Representation a base Xiaomi device."""
+
+    def __init__(self, device, name, xiaomi_hub):
+        """Initialize the xiaomi device."""
+        self._sid = device['sid']
+        self._name = '{}_{}'.format(name, self._sid)
+        self.parse_data(device['data'])
+        self.xiaomi_hub = xiaomi_hub
+        xiaomi_hub.XIAOMI_HA_DEVICES[self._sid].append(self)
+
+    @property
+    def name(self):
+        """Return the name of the device."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        return False
+
+    def push_data(self, data):
+        return True
+
+    def parse_data(self, data):
+        return True
 
 class XiaomiGenericSwitch(XiaomiDevice, SwitchDevice):
     """Representation of a XiaomiPlug."""
 
-    def __init__(self, resp, name, dataKey, xiaomiHub):
+    def __init__(self, device, name, data_key, xiaomi_hub):
         """Initialize the XiaomiPlug."""
         self._state = False
-        self._dataKey = dataKey
-        XiaomiDevice.__init__(self, resp, name, xiaomiHub)
+        self._data_key = data_key
+        XiaomiDevice.__init__(self, device, name, xiaomi_hub)
 
     @property
     def icon(self):
-        if self._dataKey == 'status':
+        if self._data_key == 'status':
            return 'mdi:power-plug'
         else:
             return 'mdi:power-socket'
@@ -47,13 +75,13 @@ class XiaomiGenericSwitch(XiaomiDevice, SwitchDevice):
     def turn_on(self, **kwargs):    
         """Turn the switch on."""
         self._state = True
-        self.xiaomiHub.write_to_hub(self._sid, self._dataKey, 'on')
+        self.xiaomi_hub.write_to_hub(self._sid, self._data_key, 'on')
         self.schedule_update_ha_state()
 
     def turn_off(self):
         """Turn the switch off."""
         self._state = False
-        self.xiaomiHub.write_to_hub(self._sid, self._dataKey, 'off')
+        self.xiaomi_hub.write_to_hub(self._sid, self._data_key, 'off')
         self.schedule_update_ha_state()
 
     def toggle(self):
@@ -63,7 +91,10 @@ class XiaomiGenericSwitch(XiaomiDevice, SwitchDevice):
             self.turn_off()
 
     def parse_data(self, data):
-        state = True if data[self._dataKey] == 'on' else False
+        if not self._data_key in data:
+            return False
+
+        state = True if data[self._data_key] == 'on' else False
         if self._state == state:
             return False
         else:
@@ -72,11 +103,9 @@ class XiaomiGenericSwitch(XiaomiDevice, SwitchDevice):
 
     def push_data(self, data):
         """Push from Hub"""
-        if self._dataKey in data and self.parse_data(data) == True:
+        if self.parse_data(data) == True:
             self.schedule_update_ha_state()
 
     def update(self):
-        _LOGGER.info('Updating')
-        data = self.xiaomiHub.get_from_hub(self._sid)
+        data = self.xiaomi_hub.get_from_hub(self._sid)
         self.push_data(data)
-        _LOGGER.info('Pushing {0}'.format(data))
